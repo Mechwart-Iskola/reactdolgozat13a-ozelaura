720a2ded66eb3f6d51fbf9f165dd3fd25431e7c4
Őze Laura <oze.laura@burg.lo.hu>
2025-01-15 09:32:38 +0100

    first commit

---
+++ src/components/Termekkereso.tsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+type Product = {
+  id: number;
+  name: string;
+  price: number;
+  category: string;
+  image: string;
+};
+
+const Termekkereso = () => {
+
+
+}
+
+export default Termekkereso;
